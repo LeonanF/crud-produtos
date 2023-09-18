8cdb85d8bdeb3bf141d1652a0f3ac3b230829d3a
Leonan <pedroleonan85@gmail.com>
2023-09-17 23:30:32 -0300

    Add formulário de nome/idade conectado ao BD online e exibindo os dados

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -17,11 +19,8 @@ type User struct {
 func main() {
 	// Crie um novo servidor Gin
 	server := gin.New()
-
-	// Configure o roteamento para servir o arquivo "index.html" como página principal
-	server.GET("/", func(c *gin.Context) {
-		c.File("static/index.html")
-	})
+	// Carregue os modelos HTML
+	server.LoadHTMLGlob("static/*")
 
 	// Conecte-se ao MongoDB
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://leonanfreitas:qtHEEIuQ84ePeavH@cluster0.2inszqq.mongodb.net/?retryWrites=true&w=majority"))
@@ -37,6 +36,34 @@ func main() {
 
 	// Coleção de usuários no MongoDB
 	collection := client.Database("users_db").Collection("users")
+
+	// Configure o roteamento para servir o arquivo "index.html" como página principal
+	server.GET("/", func(c *gin.Context) {
+		// Crie uma consulta para recuperar todos os usuários.
+		filter := bson.D{}
+
+		// Execute a consulta.
+		cursor, err := collection.Find(context.Background(), filter)
+		if err != nil {
+			c.AbortWithStatus(500)
+			return
+		}
+
+		var users []User
+
+		// Itere pelos documentos no cursor.
+		for cursor.Next(context.Background()) {
+			var user User
+			if err := cursor.Decode(&user); err != nil {
+				c.AbortWithStatus(500)
+				return
+			}
+			users = append(users, user)
+		}
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Users": users,
+		})
+	})
 
 	server.POST("/processar", func(c *gin.Context) {
 		// Obtenha o valor do campo "nome" do formulário
