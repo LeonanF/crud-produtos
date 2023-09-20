e105669a04001190777bd7463deabb85a38f1663
Leonan <pedroleonan85@gmail.com>
2023-09-19 21:59:59 -0300

    Att estilo container-prod

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,13 @@
 const arrayContainerProdutos = document.querySelectorAll('.outer-container')
 const arrayProdutos = document.querySelectorAll('.inner-container')
+const arrowImage = document.querySelectorAll('.arrow-img')
 
 arrayContainerProdutos.forEach((element, index) => {
    element.addEventListener('click', ()=>{
     arrayProdutos[index].classList.toggle('hidden')
+    arrowImage[index].classList.toggle('before:rotate-[135deg]')
+    arrowImage[index].classList.toggle('before:-rotate-45')
+    arrowImage[index].classList.toggle('before:top-[30px]') 
+    arrowImage[index].classList.toggle('before:top-[45px]') 
    }) 
 });
